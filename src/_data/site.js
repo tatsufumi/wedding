a3358310a2c22b184e0aaa89deb900a741809b52
tatsufumi <>
2022-05-15 23:50:04 +0900

    feat: meta修正

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,11 +2,11 @@ module.exports = {
   // サイト名
   title: "Tatsu and Futaba's Wedding",
   // トップページのみ付与される簡易的な説明文
-  catch: " - 佐藤家結婚式のweb招待状",
+  catch: "",
   // meta description
-  description: "樹史と二葉の結婚式のweb招待状です。11tyで作ります",
+  description: "結婚式のweb招待状を11tyで作る",
   // URL（最後の / は含めない）
-  url: "https://example.com",
+  url: "https://tatsufumi.github.io/wedding/",
   // 文字コード
   charset: "utf-8",
   // 言語
@@ -21,7 +21,7 @@ module.exports = {
   // トップページ以外の og:type
   ogType: "article",
   // og:image
-  ogImage: "assets/img/blog/share/ogp.jpg",
+  ogImage: "https://tatsufumi.github.io/wedding/assets/img/ogp.jpg",
   // Twitter Card
   twitterCard: "summary",
   // Twitter Site（空の場合出力しない） 例：@ユーザー名
